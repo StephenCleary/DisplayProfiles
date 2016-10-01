d9cbcc542ef9f91911c79979a9d5a08f045d9a18
Stephen Cleary <github.ourteddybear@xoxy.net>
2016-10-01 19:42:09 -0400

    Converted over "update adapterId" logic.

--- MonitorSwitcher/MonitorSwitcher/DisplaySettings.cs
+++ MonitorSwitcher/MonitorSwitcher/DisplaySettings.cs
@@ -38,5 +38,47 @@ namespace MonitorSwitcherGUI
             var arrays = CCDWrapper.GetDisplayConfig(flags);
             return new DisplaySettings(arrays.Item1, arrays.Item2);
         }
+
+        public DisplaySettings UpdateAdapterIds()
+        {
+            // For some reason the adapterID parameter changes upon system restart, all other parameters however, especially the ID remain constant.
+            // We check the loaded settings against the current settings replacing the adapaterID with the other parameters
+
+            var current = GetCurrent(activeOnly: false);
+            for (var i = 0; i != PathInfo.Count; ++i)
+            {
+                var path = PathInfo[i];
+                var j = current.PathInfo.FindIndex(x => x.sourceInfo.id == path.sourceInfo.id && x.targetInfo.id == path.targetInfo.id);
+                if (j == -1)
+                    continue;
+                path.sourceInfo.adapterId = current.PathInfo[j].sourceInfo.adapterId;
+                path.targetInfo.adapterId = current.PathInfo[j].targetInfo.adapterId;
+                PathInfo[i] = path;
+            }
+
+            // Same again for modeInfo, however we get the required adapterId information from the pathInfoArray
+            for (var i = 0; i != ModeInfo.Count; ++i)
+            {
+                var targetMode = ModeInfo[i];
+                var j = PathInfo.FindIndex(x => x.targetInfo.id == targetMode.id && targetMode.infoType == CCDWrapper.DisplayConfigModeInfoType.Target);
+                if (j == -1)
+                    continue;
+                var path = PathInfo[j];
+
+                // We found target adapter id, now lets look for the source modeInfo and adapterID
+                var k = ModeInfo.FindIndex(x => x.id == path.sourceInfo.id && x.adapterId == targetMode.adapterId && x.infoType == CCDWrapper.DisplayConfigModeInfoType.Source);
+                if (k != -1)
+                {
+                    var sourceMode = ModeInfo[k];
+                    sourceMode.adapterId = path.sourceInfo.adapterId;
+                    ModeInfo[k] = sourceMode;
+                }
+
+                targetMode.adapterId = path.targetInfo.adapterId;
+                ModeInfo[i] = targetMode;
+            }
+
+            return this;
+        }
     }
 }
