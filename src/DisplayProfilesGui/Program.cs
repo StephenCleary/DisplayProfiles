aa110f2ccc7ca1a997528a5e9e22bf4ef3ae6f7e
Stephen Cleary <github.ourteddybear@xoxy.net>
2016-10-07 12:57:15 -0400

    Don't allow multiple GUI instances.

--- src/DisplayProfilesGui/Program.cs
+++ src/DisplayProfilesGui/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Windows.Forms;
 using DisplayProfiles;
 using Newtonsoft.Json;
@@ -9,18 +10,26 @@ namespace DisplayProfilesGui
 {
     static class Program
     {
+        private const string ApplicationId = "DisplayProfilesE6B3BA7BF428421AAA940209D4531850";
+        
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Profile.JsonSerializerSettings.TypeNameHandling = TypeNameHandling.Auto;
-            SettingsFiles.EnsureCreated();
-            using (new MainForm())
-                Application.Run();
+            bool firstApplicationInstance;
+            using (new Mutex(false, ApplicationId, out firstApplicationInstance))
+            {
+                if (!firstApplicationInstance)
+                    return;
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Profile.JsonSerializerSettings.TypeNameHandling = TypeNameHandling.Auto;
+                SettingsFiles.EnsureCreated();
+                using (new MainForm())
+                    Application.Run();
+            }
         }
     }
 }
